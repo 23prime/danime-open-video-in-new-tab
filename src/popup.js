31e858e4a4129e59b5ece1e4889040eb9956b8cc
23prime <23.prime.37@gmail.com>
2022-05-24 00:01:21 +0900

    Set menu colors from local storage

--- src/popup.js
+++ src/popup.js
@@ -9,6 +9,39 @@ const openContactLink = () => {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("development").addEventListener("click", openDevelopmentLink);
-    document.getElementById("contact").addEventListener("click", openContactLink);
+    const targets = [
+        { elementId: "development", link: "https://github.com/23prime/danime-open-video-in-new-tab" },
+        { elementId: "contact", link: "https://twitter.com/23_prime" },
+    ];
+
+    targets.forEach((target) => {
+        const element = document.getElementById(target.elementId);
+        setMainColor(element);
+        addClickEvent(element, target.link);
+        addMouseOverEvent(element);
+        addMouseLeaveEvent(element);
+    });
 });
+
+const setMainColor = (element) => {
+    element.style.backgroundColor = localStorage.mainButtonColor;
+};
+
+const addClickEvent = (element, url) => {
+    element.addEventListener("click", () => {
+        window.open(url);
+        window.close();
+    });
+};
+
+const addMouseOverEvent = (element) => {
+    element.addEventListener("mouseover", (event) => {
+        element.style.setProperty("background-color", localStorage.hoveredButtonColor, "important");
+    });
+};
+
+const addMouseLeaveEvent = (element) => {
+    element.addEventListener("mouseleave", (event) => {
+        element.style.setProperty("background-color", localStorage.mainButtonColor, "important");
+    });
+};
